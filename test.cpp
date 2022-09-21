f1fd72e4035ba7f34abf5340f4510a21631b4f97
Riwaj Mainali <riwaj227@gmail.com>
2022-09-21 18:43:11 -0500

    error fixed

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#include"lib/typeDefine.h"
-#include<stdint.h>
 #include<vector>
+#include"lib/math/Vec.h"
 using namespace std;
+int main(){
+  Vec<2, int> v(5, 5,1); 
+  return 0;
+}
