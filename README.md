26b7fa69695816eedaf555f8ce2c6eb2b9dbef93
Riwaj Mainali <riwaj227@gmail.com>
2022-09-21 22:33:30 -0500

    change of plans
    Using python and C++ instead of only C++

--- README.md
+++
@@ -1,5 +0,0 @@
-# Picograd
-
-For something smaller than tinygrad and nanograd
-
-Uses Vulkan in C++ for GPU accelaration. Python bindings in the future
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include<vector>
-#include"lib/math/Vec.h"
-using namespace std;
-int main(){
-  Vec<2, int> v(5, 5,1); 
-  return 0;
-}
